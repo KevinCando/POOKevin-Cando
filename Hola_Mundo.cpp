fca6cfdfb8c3b2067b6fe9ecde2c3323826cd45b
GabrielCaicedo <62907581+GabrielCaicedo@users.noreply.github.com>
2020-05-31 19:42:33 -0500

    Update Hola_Mundo.cpp

--- Hola_Mundo.cpp
+++ Hola_Mundo.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include<stdlib.h>
+#include<stdio.h>
 using namespace std;
 int main()
 {
-    cout<<"Hola Mundo"<<endl;
-    cout<<"Programacion Orientada a Objetos"<<endl;
+    char Frase[1];
+    cout<<"Ingrese un Mensaje:  ";
+    gets(Frase);
+    cout<<Frase<<endl;
     return 0;
 }
